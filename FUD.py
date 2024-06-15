01ebee405bf00bb26e5408ff6715cb7c5d8704c4
mQueue <tomorrow9913@gmail.com>
2024-06-15 22:24:00 +0900

    Feat: 와일드 카드로 패키지 import 하는 경우 제거

--- FUD.py
+++ FUD.py
@@ -12,20 +12,35 @@ def get_import_list(formatter_jar, file):
     # 파일을 읽을 때 인코딩을 명시적으로 지정
     with open(file, "r", encoding="utf-8") as f:
         file_import += [line.strip() for line in f if line.strip().startswith("import")]
-    
-    return file_import
+
+    return list(map(lambda x: x.replace("import ", "").replace(";", ""), file_import))
 
 
 def del_unused_import(formatter_jar, file):
     return subprocess.run(["java", "-jar", formatter_jar, "--replace", file])
 
 
+def get_wildcard_import(import_statement):
+    if import_statement.endswith(".*"):
+        return import_statement
+    else:
+        parts = import_statement.split('.')
+        if len(parts) > 1:
+            parts.pop()
+            return '.'.join(parts) + '.*'
+        else:
+            return import_statement + '.*'
+
+
 def list_of_unused_import(all_import, used_import):
     unused_imports = set()
-
     for import_line in all_import:
-        if import_line not in used_import:
+        if get_wildcard_import(import_line) not in used_import and import_line not in used_import:
             unused_imports.add(import_line)
+
+    for import_line in unused_imports:
+        if get_wildcard_import(import_line) in used_import:
+            unused_imports.remove(import_line)
 
     return list(unused_imports)
 
